d3cd1e2a271c2f16716142fc47897e98c106cdf8
Denis Zorin <dzorin@alfabank.ru>
2018-09-21 15:17:19 +0300

    1

--- dz-2/src/app/components/username.jsx
+++ dz-2/src/app/components/username.jsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+const fio = 'Zorin Denis';
+
 class JUserName extends React.Component {
-  state = {text: 'Zorin Denis'}
+  state = {text: fio, mode: 1}
   constructor(){
     super();
     this.doClick = this.doClick.bind(this);
   }
   doClick(){
     let dt = 'Current date: ' + new Date();
-    this.setState({text: dt});
+    this.setState({text: (this.state.mode == 1)?dt:fio, mode: (this.state.mode == 1)?0:1});
   }
   render() {
     return (
